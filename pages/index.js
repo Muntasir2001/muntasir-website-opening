7fe566ca5e2a88440400429d1f89487f4d7b6220
Muntasir <muntasirenamalhuda@gmail.com>
2021-03-15 17:37:15 +0000

    created comingsoon page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,28 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Noob Dev Official</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
+      <div className={styles['nd-logo']}>
+        <img src="/ndsq-blue.svg" alt="Logo" />
+      </div>
+      <div className={styles['nd-text']}>
+        <h1>hey, i am <span>noob dev</span></h1>
+        <p>official website coming soon</p>
+      </div>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div className={styles.icons}>
+          <img src="/fb-icon.svg" alt="fb-icon" />
+          <img src="/insta-icon.svg" alt="insta-icon" />
+          <img src="/linkedin-icon.svg" alt="linkedin-icon" />
+          <img src="/github.svg" alt="github-icon" />
+        </div>
+        <p>Copyright ©2021 All rights reserved</p>
       </footer>
-    </div>
+    </>
   )
 }
